abf3be2de749ea4b64e8876b2fa4bd84d8fb050c
Alexey Deryugin <aderyugin@mirantis.com>
2015-11-10 19:55:52 +0300

    Switch to library-type implementation
    Change-Id: I4b584736c9ae46cb0e6f7a99e9985d173642dee7

--- src/ru/linachan/nemesis/NemesisEventListener.java
+++ src/ru/linachan/nemesis/NemesisEventListener.java
@@ -1,12 +1,9 @@
 package ru.linachan.nemesis;
 
-import ru.linachan.bifrost.BifrostPlugin;
-import ru.linachan.bifrost.BifrostSSHAuth;
 import ru.linachan.bifrost.BifrostSSHConnection;
 import ru.linachan.yggdrasil.service.YggdrasilService;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 
 public class NemesisEventListener extends YggdrasilService {
@@ -15,26 +12,7 @@ public class NemesisEventListener extends YggdrasilService {
 
     @Override
     protected void onInit() {
-        try {
-            String serverHost = core.getConfig("NemesisGitHost", "localhost");
-            Integer serverPort = Integer.parseInt(core.getConfig("NemesisGitPort", "29418"));
-            String serverUser = core.getConfig("NemesisSSHUser", "gerrit2");
-            File serverKey = new File(core.getConfig("NemesisSSHKey", "/root/.ssh/id_rsa"));
-            String serverPassPhrase = core.getConfig("NemesisSSHPassPhrase", "");
-
-            BifrostSSHAuth serverAuth;
-            if (serverPassPhrase.length() > 0) {
-                serverAuth = new BifrostSSHAuth(serverKey, serverUser, serverPassPhrase);
-            } else {
-                serverAuth = new BifrostSSHAuth(serverKey, serverUser);
-            }
-
-            core.logInfo("BifrostEventListener: Connecting: ssh://" + serverUser + "@" + serverHost + ":" + serverPort);
-
-            serverLink = core.getPluginManager().getPlugin(BifrostPlugin.class).connect(serverHost, serverPort, serverAuth);
-        } catch (IOException e) {
-            core.logException(e);
-        }
+        serverLink = core.getPluginManager().getPlugin(NemesisPlugin.class).getConnection();
     }
 
     @Override
@@ -46,20 +24,24 @@ public class NemesisEventListener extends YggdrasilService {
 
     @Override
     public void run() {
-        try {
-            BufferedReader eventReader = new BufferedReader(serverLink.executeCommandReader("gerrit stream-events"));
+        if (serverLink != null) {
+            try {
+                BufferedReader eventReader = new BufferedReader(serverLink.executeCommandReader("gerrit stream-events"));
 
-            while (isRunning()) {
-                String eventData = eventReader.readLine();
-                if (eventData != null && eventData.length() > 0) {
-                    NemesisEvent event = new NemesisEvent(eventData);
+                while (isRunning()) {
+                    String eventData = eventReader.readLine();
+                    if (eventData != null && eventData.length() > 0) {
+                        NemesisEvent event = new NemesisEvent(eventData);
 
-                    core.getPluginManager().getPlugin(NemesisPlugin.class).putEvent(event);
+                        core.getPluginManager().getPlugin(NemesisPlugin.class).putEvent(event);
+                    }
                 }
-            }
 
-        } catch (IOException e) {
-            core.logException(e);
+            } catch (IOException e) {
+                core.logException(e);
+            }
+        } else {
+            core.logWarning("NemesisEventListener: No connection available");
         }
     }
 }
--- src/ru/linachan/nemesis/NemesisPlugin.java
+++ src/ru/linachan/nemesis/NemesisPlugin.java
@@ -1,14 +1,22 @@
 package ru.linachan.nemesis;
 
 import ru.linachan.bifrost.BifrostPlugin;
+import ru.linachan.bifrost.BifrostSSHAuth;
+import ru.linachan.bifrost.BifrostSSHConnection;
 import ru.linachan.yggdrasil.component.YggdrasilPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class NemesisPlugin extends YggdrasilPlugin {
 
     private Queue<NemesisEvent> eventQueue;
+
+    private String serverHost;
+    private Integer serverPort;
+    private BifrostSSHAuth serverAuth;
 
     @Override
     protected void setUpDependencies() {
@@ -19,8 +27,20 @@ public class NemesisPlugin extends YggdrasilPlugin {
     protected void onInit() {
         eventQueue = new PriorityQueue<>();
 
+        serverHost = core.getConfig("NemesisGitHost", "localhost");
+        serverPort = Integer.parseInt(core.getConfig("NemesisGitPort", "29418"));
+
+        String serverUser = core.getConfig("NemesisSSHUser", "gerrit2");
+        File serverKey = new File(core.getConfig("NemesisSSHKey", "/root/.ssh/id_rsa"));
+        String serverPassPhrase = core.getConfig("NemesisSSHPassPhrase", "");
+
+        if (serverPassPhrase.length() > 0) {
+            serverAuth = new BifrostSSHAuth(serverKey, serverUser, serverPassPhrase);
+        } else {
+            serverAuth = new BifrostSSHAuth(serverKey, serverUser);
+        }
+
         core.getServiceManager().startService(new NemesisEventListener());
-        core.getServiceManager().startService(new NemesisEventHandler());
     }
 
     @Override
@@ -39,5 +59,51 @@ public class NemesisPlugin extends YggdrasilPlugin {
 
     public NemesisEvent getEvent() {
         return (!eventQueue.isEmpty()) ? eventQueue.remove() : null;
+    }
+
+    public BifrostSSHConnection getConnection() {
+        try {
+            return core.getPluginManager().getPlugin(BifrostPlugin.class).connect(serverHost, serverPort, serverAuth);
+        } catch (IOException e) {
+            core.logException(e);
+            return null;
+        }
+    }
+
+    public void reviewChange(NemesisEvent event, NemesisApprovalType approval, Integer value, boolean trySubmit, String comment) {
+        try {
+            BifrostSSHConnection serverLink = getConnection();
+
+            Integer changeNumber = event.getChangeRequest().getChangeNumber();
+            Integer patchSetNumber = event.getPatchSet().getPatchSetNumber();
+            String approvalOption;
+
+            switch(approval) {
+                case CODE_REVIEW:
+                    approvalOption = "--code-review";
+                    break;
+                case VERIFIED:
+                    approvalOption = "--verified";
+                    break;
+                case WORKFLOW:
+                    approvalOption = "--workflow";
+                    break;
+                default:
+                    approvalOption = "";
+                    break;
+            }
+
+            String approvalCommand = "gerrit review";
+            approvalCommand += " " + changeNumber + "," + patchSetNumber;
+            approvalCommand += " " + approvalOption + " " + value;
+            approvalCommand += (comment != null) ? " --comment " + comment + " ": "";
+            approvalCommand += (trySubmit) ? " --submit" : "";
+
+            core.logInfo("NemesisEvent: Voting " + approval.toString() + ": " + value + " for " + changeNumber + "," + patchSetNumber);
+
+            serverLink.executeCommand(approvalCommand);
+        } catch (IOException e) {
+            core.logException(e);
+        }
     }
 }
